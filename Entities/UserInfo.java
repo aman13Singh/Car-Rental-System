981311b7ba5a3fc1d3fb53096a92fd2720f1af16
Amanpreet Singh <singha73@student.douglascollege.ca>
2019-11-16 01:28:25 -0800

    Add new file userinfo.java

---
+++ Entities/UserInfo.java
@@ -0,0 +1,34 @@
+package Entities;
+
+public class UserInfo {
+
+	private int customerID;
+	private String customerName;
+	/**
+	 * @return the customerID
+	 */
+	public int getCustomerID() {
+		return customerID;
+	}
+	/**
+	 * @param customerID the customerID to set
+	 */
+	public void setCustomerID(int customerID) {
+		this.customerID = customerID;
+	}
+	/**
+	 * @return the customerName
+	 */
+	public String getCustomerName() {
+		return customerName;
+	}
+	/**
+	 * @param customerName the customerName to set
+	 */
+	public void setCustomerName(String customerName) {
+		this.customerName = customerName;
+	}
+
+	
+	
+}
