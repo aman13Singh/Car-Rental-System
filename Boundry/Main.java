eacc2276f9e2c5eaaa04b918495e3671d51674e7
Amanpreet Singh <singha73@student.douglascollege.ca>
2019-11-28 23:13:04 -0800

    Update the connection between classes

--- Boundry/Main.java
+++ Boundry/Main.java
@@ -61,7 +61,7 @@ public class Main {
 		JLabel lblCarRegistration = new JLabel("Car Registration");
 		lblCarRegistration.setForeground(SystemColor.activeCaption);
 		lblCarRegistration.setFont(new Font("Tahoma", Font.BOLD, 15));
-		lblCarRegistration.setBounds(10, 160, 155, 34);
+		lblCarRegistration.setBounds(46, 136, 155, 83);
 		frame.getContentPane().add(lblCarRegistration);
 		
 		JButton btnCarRegistration = new JButton("Car Registration");
@@ -72,73 +72,63 @@ public class Main {
 		});
 		btnCarRegistration.setForeground(SystemColor.activeCaption);
 		btnCarRegistration.setFont(new Font("Tahoma", Font.BOLD, 15));
-		btnCarRegistration.setBounds(10, 230, 155, 23);
+		btnCarRegistration.setBounds(36, 230, 165, 23);
 		frame.getContentPane().add(btnCarRegistration);
-		
-		JLabel lblCustomer = new JLabel("Customer");
-		lblCustomer.setForeground(SystemColor.activeCaption);
-		lblCustomer.setFont(new Font("Tahoma", Font.BOLD, 15));
-		lblCustomer.setBounds(167, 160, 103, 34);
-		frame.getContentPane().add(lblCustomer);
-		
-		JButton btnCustomer = new JButton("Customer");
-		btnCustomer.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent e) {
-				CustomerDAO.main(null);
-			}
-		});
-		btnCustomer.setForeground(SystemColor.activeCaption);
-		btnCustomer.setFont(new Font("Tahoma", Font.BOLD, 15));
-		btnCustomer.setBounds(177, 230, 103, 23);
-		frame.getContentPane().add(btnCustomer);
 		
 		JLabel lblRentCar = new JLabel("Rent  Car");
 		lblRentCar.setForeground(SystemColor.activeCaption);
 		lblRentCar.setFont(new Font("Tahoma", Font.BOLD, 15));
-		lblRentCar.setBounds(290, 160, 123, 34);
+		lblRentCar.setBounds(253, 136, 123, 83);
 		frame.getContentPane().add(lblRentCar);
 		
 		JButton btnRental = new JButton("Rental");
 		btnRental.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
-				RentCarDAO.main(null);
+				RentCarDAO.main(null, user);
 			}
 		});
 		btnRental.setForeground(SystemColor.activeCaption);
 		btnRental.setFont(new Font("Tahoma", Font.BOLD, 15));
-		btnRental.setBounds(290, 230, 123, 23);
+		btnRental.setBounds(253, 230, 123, 23);
 		frame.getContentPane().add(btnRental);
 		
 		JLabel lblReturnCar = new JLabel("Return Car");
 		lblReturnCar.setForeground(SystemColor.activeCaption);
 		lblReturnCar.setFont(new Font("Tahoma", Font.BOLD, 15));
-		lblReturnCar.setBounds(436, 160, 115, 34);
+		lblReturnCar.setBounds(417, 136, 115, 83);
 		frame.getContentPane().add(lblReturnCar);
 		
 		JButton btnReturnCar = new JButton("Return Car");
 		btnReturnCar.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
-				ReturnCarDAO.main(null);
+				ReturnCarDAO.main(null, user);
 			}
 		});
 		btnReturnCar.setForeground(SystemColor.activeCaption);
 		btnReturnCar.setFont(new Font("Tahoma", Font.BOLD, 15));
-		btnReturnCar.setBounds(436, 230, 115, 23);
+		btnReturnCar.setBounds(417, 230, 115, 23);
 		frame.getContentPane().add(btnReturnCar);
 		
 		JLabel lblWelcome = new JLabel("Welcome ");
 		lblWelcome.setForeground(SystemColor.activeCaption);
 		lblWelcome.setFont(new Font("Tahoma", Font.BOLD, 15));
-		lblWelcome.setBounds(135, 27, 213, 89);
+		lblWelcome.setBounds(36, 11, 213, 89);
 		frame.getContentPane().add(lblWelcome);
 		lblWelcome.setText("Welcome " + user.getCustomerName());
 		System.out.println(user.getCustomerName());
 
 		
 		JButton btnSignOut = new JButton("Sign Out");
+		btnSignOut.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				
+				Login.main(null);
+				frame.dispose();
+			}
+		});
 		btnSignOut.setForeground(SystemColor.activeCaption);
 		btnSignOut.setFont(new Font("Tahoma", Font.BOLD, 15));
-		btnSignOut.setBounds(436, 327, 115, 23);
+		btnSignOut.setBounds(432, 327, 115, 23);
 		frame.getContentPane().add(btnSignOut);
 	}
 }
