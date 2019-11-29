7dfb07bc889ed0049c244267de87f6f69929ccf4
Amanpreet Singh <singha73@student.douglascollege.ca>
2019-11-28 23:30:05 -0800

    add new class  CarRegistration.java

--- Entities/CarRegistration.java
+++ Entities/CarRegistration.java
@@ -4,6 +4,7 @@
 package Entities;
 
 import java.sql.Date;
+import java.util.Vector;
 
 /**
  * @author Amanpreet Singh
@@ -11,13 +12,26 @@ import java.sql.Date;
  */
 public class CarRegistration {
 
+	private int ID;
 	private String CarRegistrationNo;
 	private String model;
 	private String make;
 	private Date fromDate;
 	private Date toDate;
+	private float price;
 	
-	
+	/**
+	 * @return the iD
+	 */
+	public int getID() {
+		return ID;
+	}
+	/**
+	 * @param iD the iD to set
+	 */
+	public void setID(int iD) {
+		ID = iD;
+	}
 	/**
 	 * @return the carRegistrationNo
 	 */
@@ -79,6 +93,72 @@ public class CarRegistration {
 		this.toDate = toDate;
 	}
 
+
+	/**
+	 * @return the price
+	 */
+	public float getPrice() {
+		return price;
+	}
+	/**
+	 * @param price the price to set
+	 */
+	public void setPrice(float price) {
+		this.price = price;
+	}
 	
+	public Vector getVector() {
+		Vector v = new Vector();
+		
+		// CarRegistrationNo, model , make, fromDate, toDate
+
+		
+		v.add(CarRegistrationNo);
+		v.add(model);
+		v.add(make);
+		v.add(fromDate);
+		v.add(toDate);
+		v.add(price);
+	
+		
+		return v;
+	}
+	
+	
+	public Vector getVectSel() {
+		Vector v = new Vector();
+		
+		// CarRegistrationNo, model , make, fromDate, toDate
+
+		
+	//	v.add(CarRegistrationNo);
+		v.add(model);
+		v.add(make);
+		v.add(price);
+	//	v.add(fromDate);
+	//	v.add(toDate);
+	
+		
+		return v;
+	}
+	
+	
+	
+	public Vector getVectoradmin() {
+		Vector v = new Vector();
+		
+		// CarRegistrationNo, model , make, fromDate, toDate
+
+		v.add(ID);
+		v.add(CarRegistrationNo);
+		v.add(model);
+		v.add(make);
+		v.add(fromDate);
+		v.add(toDate);
+		v.add(price);
+	
+		
+		return v;
+	}
 	
 }
