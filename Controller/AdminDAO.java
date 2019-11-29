12349888a88889cb43156d760bf37e7335fedc2c
Amanpreet Singh <singha73@student.douglascollege.ca>
2019-11-28 23:27:15 -0800

    Allow the admin to setup admin  username and password

---
+++ Controller/AdminDAO.java
@@ -0,0 +1,93 @@
+/**
+ * 
+ */
+package Controller;
+
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+
+import Boundry.DBHelper;
+
+/**
+ * @author Amanpreet Singh
+ *
+ */
+
+public class AdminDAO {
+	DBHelper helper = new DBHelper();
+	private ResultSet rs = null;
+	private Statement stmt = null;
+	private PreparedStatement pstmt = null;
+
+
+	public AdminDAO() {
+		
+	}
+	
+	/**
+	  * @return list of usernames
+	  */
+		public ArrayList<String> listAdminUserNames() 
+		{
+			ArrayList<String> s1 = new ArrayList<String>();
+
+			String sql = "SELECT customerName FROM user_Info";
+			try {
+				// connect to the database
+				helper.connectDB();
+				this.stmt = helper.getConnection().createStatement();
+				rs = stmt.executeQuery(sql);
+
+				while (rs.next())
+				{
+					s1.add(rs.getString("customerName"));				
+				}
+
+				helper.disconnectDB();
+			} catch (SQLException sx) {
+				System.out.println("Error fetching data from the database");
+				System.out.println(sx.getMessage());
+				System.out.println(sx.getErrorCode());
+				System.out.println(sx.getSQLState());
+			}
+
+			return s1;
+		}
+		
+		
+		/**
+		  * 
+		  * @param usernm, username for whom password is being fetched
+		  * @return password for specific user
+		  */
+		public String getAdminPassword(String customerName) {
+
+			String s1 = "";
+			String sql = "SELECT password FROM user_Info where customerName = '" + customerName + "'";
+			try {
+				// connect to the database
+				helper.connectDB();
+				this.stmt = helper.getConnection().createStatement();
+				rs = stmt.executeQuery(sql);
+
+				while (rs.next())
+				{			
+					s1 = rs.getString(1);
+				}
+
+				helper.disconnectDB();
+			} catch (SQLException sx) {
+				System.out.println("Error fetching data from the database");
+				System.out.println(sx.getMessage());
+				System.out.println(sx.getErrorCode());
+				System.out.println(sx.getSQLState());
+			}
+
+			return s1;
+		}
+
+}
+
